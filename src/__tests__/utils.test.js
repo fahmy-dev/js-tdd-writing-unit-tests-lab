8b33b5f3d998ad1990f60c494549c51e8acb0bb4
fahmy-dev <faahmy@proton.me>
2025-01-28 17:35:36 +0300

    initial commit

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,21 @@
 // Your tests here
+import { isPalindrome } from "../utils";
+
+describe("isPalindrome", () => {
+  it('returns true for "racecar"', () => {
+    expect(isPalindrome('racecar')).toBe(true);
+  });
+
+  it('returns false for "car"', () => {
+    expect(isPalindrome('car')).toBe(false);
+  });
+
+  it('returns false for empty string', () => {
+    expect(isPalindrome('')).toBe(false);
+  });
+
+  it('throws error for non-alphabetic characters', () => {
+    expect(() => isPalindrome('race car')).toThrow();
+    expect(() => isPalindrome('12321')).toThrow();
+  });
+});
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,14 @@
 // Your code here
+export function isPalindrome(word) {
+    if (!/^[A-Za-z]+$/.test(word)) {
+        throw new Error('Input must contain only alphabetical characters');
+    }
+    
+    if (word === '') {
+        return false;
+      }
+    
+    const lowerWord = word.toLowerCase();
+    const reversedWord = lowerWord.split('').reverse().join('')
+    return lowerWord === reversedWord
+}
